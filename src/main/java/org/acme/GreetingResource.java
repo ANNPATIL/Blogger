67f4b76612444fc7eb55dff6035e4c2cceed326c
annpatil <annpatil@redhat.com>
2021-12-07 18:09:19 +0530

    second commit

--- src/main/java/org/acme/GreetingResource.java
+++ src/main/java/org/acme/GreetingResource.java
@@ -3,13 +3,26 @@ package org.acme;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 
-@Path("/blog")
+@Path("/ABC")
 public class GreetingResource {
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
-    public String blog() {
+    public String ABC() {
         return "Read All Blogs";
+    }
+
+
+    @GET
+    @Path("/{name}/{All}")
+    public String All(@PathParam("name") String name, @PathParam("All") String All) {
+        return "Hello" +name ;
+    }
+
+    @GET
+    @Path("/particular")
+    public String particular() {
+        return "Get a particular blog";
     }
 
     @POST
@@ -19,13 +32,11 @@ public class GreetingResource {
     }
 
     @PUT
-    @Path("/ABC")
-    public String ABC() {
-        return "A blog ABC has been updated with content hello";
+    @Path("/{id}/{adding}")
+    public String adding(@PathParam("id") String id, @PathParam("hello") String hello) {
+        return "A blog with id " +id ;
+        return "has been updated with content" + hello;
     }
-    @DELETE
-    @Path("/Def")
-    public String Def(){
-        return "delete";
-    }
+
 }
+
